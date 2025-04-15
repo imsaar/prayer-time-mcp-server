ad7156fc3b8d5861b9976f9cd63dd0e3557c39c6
Ali Rizvi <arizvi@avvo.com>
2025-04-15 14:39:35 -0700

    updated readme with get_daily_prayer_times documentation

--- src/praytime.d.ts
+++ src/praytime.d.ts
@@ -1,8 +1,8 @@
-declare class PrayTimes {
-  constructor(method?: string);
-  // Add other method signatures as needed
-  getTimes(date: Date, coords: number[], timezone?: number, dst?: number, format?: string): any;
-  // Add other methods you use...
-}
+// declare class PrayTimes {
+//   constructor(method?: string);
+//   // Add other method signatures as needed
+//   getTimes(date: Date, coords: number[], timezone?: number, dst?: number, format?: string): any;
+//   // Add other methods you use...
+// }
 
-export default PrayTimes;
+// export default PrayTimes;
